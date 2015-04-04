3a2f84b7b23b417e63d9e82409ad884e2fbf4ab1
yufieko <yufieko@gmail.com>
2015-04-05 05:09:37 +0700

    update language file untuk indonesia

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,7 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends MY_Controller {
 
 	public function index() {
-		$this->load->view('Backend/login_view');
+		$this->load->view('Backend/header_view');
+		$this->load->view('Backend/dashboard_view');
 	}
 
 }
